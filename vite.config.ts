e49356f73d324c2987c96f8ece4e091c75b93cc3
fsabre <fabien.sabre@gmail.com>
2023-07-09 19:59:19 +0200

    build: Specify the build config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    // Config for GitHub Pages
+    base: "/xana-eye-generator/",
+    build: {outDir: "docs"},
+    plugins: [react()],
 })
