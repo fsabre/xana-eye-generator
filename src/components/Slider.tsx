4495bc468e9125c117ceac497c56760c934bd97b
fsabre <fabien.sabre@gmail.com>
2023-09-14 19:18:59 +0200

    feat: Allow to edit slider values as text

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -18,6 +18,7 @@ const DEFAULT_PROPS: Partial<ISliderProps> = {
 // A component to have a custom slider for the whole app
 export const Slider: React.FC<ISliderProps> = (props) => {
     props = {...DEFAULT_PROPS, ...props};
+
     return (
         <div className={"Slider config-shape-control"}>
             <label>{props.label}</label>
@@ -32,7 +33,18 @@ export const Slider: React.FC<ISliderProps> = (props) => {
                     props.onChange(Number(value));
                 }}
             />
-            <label>{props.value}</label>
+            <input
+                type={"text"}
+                disabled={props.disabled}
+                value={props.value.toFixed()}
+                onChange={(ev) => {
+                    const value = Number(ev.target.value); // Will be 0 if "" or undefined, NaN if random chars
+                    if (!isNaN(value)) {
+                        // I'm using defaults on min and max for typing, but they'll never be undefined.
+                        props.onChange(Math.min(Math.max(props.min || 0, value), props.max || 0));
+                    }
+                }}
+            />
         </div>
     );
 }
